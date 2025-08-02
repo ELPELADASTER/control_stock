ea6a888be62ee9c502d5928d1e85b7566f4aacc9
Uriel <94861@sistemas.frc.utn.edu.ar>
2025-08-02 20:23:02 -0300

    coso 2

--- multerCloudinary.js
+++ multerCloudinary.js
@@ -1,6 +1,4 @@
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('multer');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -11,17 +9,5 @@ cloudinary.config({
 console.log('Cloudinary config:', {
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET ? '***' : undefined
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'articulos', // Carpeta en Cloudinary
-    allowed_formats: ['jpg', 'png', 'jpeg'],
-  },
-});
-
-const upload = multer({ storage: storage });
-
-module.exports = upload;
